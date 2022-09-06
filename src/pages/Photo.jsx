170ac389ad1980292845005786f9a8fd5c609e19
Olga Zabalueva <olyaolya771@gmail.com>
2022-09-06 15:54:34 +0500

    Slider

--- src/pages/Photo.jsx
+++ src/pages/Photo.jsx
@@ -1,11 +1,35 @@
-import React from 'react';
+import React from "react";
+import Slider from "react-slick";
+import cat from '../styles/img/cat.jpg';
+import cat2 from '../styles/img/cat2.jpg';
+import cat3 from '../styles/img/cat3.jpg';
 
-const Photo = () => {
+export default function Photo() {
+    let settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        adaptiveHeight: true,
+        arrows: true,
+        centerPadding: '200px',
+    };
     return (
-        <h1>
-            Здесь будут фото
-        </h1>
-    );
-};
+        <div>
+            <Slider {...settings}>
+                <div>
+                    <img src={cat} alt="" className="slide-single"/>
+                </div>
+                <div>
+                    <img src={cat2} alt="" className="slide-single"/>
+                </div>
+                <div>
+                    <img src={cat3} alt="" className="slide-single"/>
+                </div>
+            </Slider>
+        </div>
 
-export default Photo;
+    );
+}
+// export default Photo;
